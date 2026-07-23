c718ce0cbd6cc02853a537f33731a3b66e48827a
natalibogarin <natali.bogarin@gmail.com>
2026-07-23 12:33:20 -0300

    Actualizado script

--- js/modules/bootstrap-ui.js
+++ js/modules/bootstrap-ui.js
@@ -1,7 +1,28 @@
 export const initBootstrapUi = () => {
+    const bootstrapLib = window.bootstrap;
+
+    if (!bootstrapLib) {
+        return;
+    }
+
     const tooltipList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    tooltipList.forEach((el) => new bootstrap.Tooltip(el));
+    tooltipList.forEach((el) => new bootstrapLib.Tooltip(el));
 
     const popoverList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
-    popoverList.forEach((el) => new bootstrap.Popover(el));
+    popoverList.forEach((el) => new bootstrapLib.Popover(el));
+
+    const carouselList = [].slice.call(document.querySelectorAll('.carousel'));
+    carouselList.forEach((el) => {
+        if (!el || el.dataset.bsCarouselInitialized === 'true') {
+            return;
+        }
+
+        const interval = Number(el.dataset.bsInterval || 4500);
+        new bootstrapLib.Carousel(el, {
+            interval,
+            ride: 'carousel',
+        });
+
+        el.dataset.bsCarouselInitialized = 'true';
+    });
 };
